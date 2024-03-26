1d20b4d420a96f46837e13df5e0e0c0eb0bb7639
marnvel <marnvel.24@gmail.com>
2024-03-25 23:49:21 -0300

    Se inicia codificación. 25-03-2024. 23_49hs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,77 @@
 ﻿
+
+string? leerResultado = null;
+bool usarDatosPrueba = false;
+
+Console.Clear();
+
+int[] dineroEnCaja = [ 0, 0, 0, 0 ];
+int registroRevisionTotalCaja = 0;
+
+// registroDineroDiarioInicial: $1 x 50, $5 x 20, $10 x 10, $20 x 5 => ($350 total)
+int[,] registroDineroDiarioInicial = new int[,] { { 1, 50 }, { 5, 20 }, { 10, 10 }, { 20, 5 } };
+
+int[] datosPrueba = [ 6, 10, 17, 20, 31, 36, 40, 41 ];
+int contadorPrueba = 0;
+
+CargarCajaCadaManana(registroDineroDiarioInicial, dineroEnCaja);
+
+registroRevisionTotalCaja = registroDineroDiarioInicial[0, 0] * registroDineroDiarioInicial[0, 1] + registroDineroDiarioInicial[1, 0] * registroDineroDiarioInicial[1, 1] + registroDineroDiarioInicial[2, 0] * registroDineroDiarioInicial[2, 1] + registroDineroDiarioInicial[3, 0] * registroDineroDiarioInicial[3, 1];
+
+// mostrar el número de billetes de cada denominación actualmente en la caja.
+ImprimirEstadoCaja(dineroEnCaja);
+
+// mostrar un mensaje que indique la cantidad de efectivo en la caja
+Console.WriteLine(ResumenCantidadEnCaja(dineroEnCaja));
+
+// mostrar el total esperado de registroDineroDiarioInicial
+Console.WriteLine($"Valor esperado en caja: {registroRevisionTotalCaja}");
+Console.WriteLine();
+
+var generadorValor = new Random((int)DateTime.Now.Ticks);
+
+int transacciones = 100;
+
+if (usarDatosPrueba)
+{
+    transacciones = datosPrueba.Length;
+}
+
+
+Console.WriteLine("Presione la tecla Enter para salir");
+do
+{
+    leerResultado = Console.ReadLine();
+
+} while (leerResultado == null);
+
+
+
+static string ResumenCantidadEnCaja(int[] dineroEnCaja)
+{
+    return $"La caja tiene {dineroEnCaja[3] * 20 + dineroEnCaja[2] * 10 + dineroEnCaja[1] * 5 + dineroEnCaja[0]} pesos";
+}
+
+static void ImprimirEstadoCaja(int[] dineroEnCaja)
+{
+    Console.WriteLine("La caja tiene actualmente:");
+    Console.WriteLine($"{dineroEnCaja[3] * 20} en billetes de veinte");
+    Console.WriteLine($"{dineroEnCaja[2] * 10} en billetes de diez");
+    Console.WriteLine($"{dineroEnCaja[1] * 5} en billetes de cinco");
+    Console.WriteLine($"{dineroEnCaja[0]} en billetes de uno");
+    Console.WriteLine();
+}
+
+static void CargarCajaCadaManana(int[,] registroDineroDiarioInicial, int[] dineroEnCaja)
+{
+    dineroEnCaja[0] = registroDineroDiarioInicial[0, 1];
+    dineroEnCaja[1] = registroDineroDiarioInicial[1, 1];
+    dineroEnCaja[2] = registroDineroDiarioInicial[2, 1];
+    dineroEnCaja[3] = registroDineroDiarioInicial[3, 1];
+}
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,57 @@
+# Juego de consola de interfaz básica con `C#`
+
+> Este proyecto se trata de una simple aplicación de consola creada con el **framework NET**.
+>
+> El objetivo de este proyecto es realizar prácticas para aprender **C#** y el marco de
+> trabajo **.NET-CORE**.
+
+## Sobre la APP
+
+> Esta es una sencilla aplicación de consola que consiste en la "gesión del vuelto" de dinero de una caja registradora en un comercio. Las principales características son:
+>
+
+## About the APP
+
+## Tecnologías utilizadas:
+<div align="center" style="display: flex">
+      <span>
+         <a href="https://learn.microsoft.com/en-us/dotnet/csharp/" target="_blank">
+               <img width="100" style="margin: 10" title='cSharp' src='https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png'>
+         </a>
+      </span>
+      <span>
+         <a href="https://learn.microsoft.com/en-us/dotnet/" target="_blank" title='dotNET'>
+               <img width="100" style="margin: 10" title='dotNET' src='https://upload.wikimedia.org/wikipedia/commons/e/ee/.NET_Core_Logo.svg'>
+         </a>
+      </span>
+</div>
+
+## Requerimientos
+
+- Tener intalado el sdk **dotNET 8** (<https://dotnet.microsoft.com/en-us/download>).
+- Tener instalado **Git**.
+
+## Usos
+
+### Para ejecutar o utilizar el proyecto debes clonar este repositorio en un directorio de tu sistema. Abre una terminal y ejecuta el siguiente comando
+
+```bash
+git clone https://github.com/MARnVEL/cajaRegistradora-ManejoExcepciones.git
+```
+
+### Con una terminal localizarse en el directorio del proyecto que contiene el fichero `Program.cs`
+
+> De acuerdo a tu sistema podría variar, pero el directorio se parece a lo siguiente:
+> `C:<direcotrio_donde_se_clono_el_proyecto>\cajaRegistradora-ManejoExcepciones\`
+
+### Ejecutar
+
+```bash
+dotnet build
+```
+
+#### Se crearán un par de directorios con los binarios necesarios para la correcta ejecución del sofware
+
+### Para correr el proyecto debes dirigirte al directorio `C:<direcotrio_donde_se_clono_el_proyecto>\cajaRegistradora-ManejoExcepciones\bin\Debug\net8.0\`
+
+### Abrir/Ejecutar el fichero `cajaRegistradora-ManejoExcepciones.exe`
